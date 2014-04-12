063fcc1d9ae5b09e147d7bcc0fe95a49137deb19
Jeff Hung <jeff.cc.hung@gmail.com>
2014-04-12 22:09:19 +0800

    Add git-hub formula (for testing).

---
+++ git-hub.rb
@@ -0,0 +1,18 @@
+require 'formula'
+
+class GitHub < Formula
+  head 'https://github.com/ingydotnet/git-hub.git', :branch => 'master'
+  homepage 'https://github.com/ingydotnet/git-hub'
+# md5 '1234567890ABCDEF1234567890ABCDEF'
+
+  def patches
+#   DATA
+  end
+
+  def install
+    system "make", "PREFIX=#{prefix}", "INSTALL_LIB=#{prefix}/bin", "install"
+  end
+end
+
+__END__
+
